91b9e6f09b4868fabd5bdb9a4b6c66676a0c4e9f
Maciej Brzezinski <brzezinski.maciej@wp.pl>
2026-04-29 09:46:08 +0200

    fix: use Attribute\Route instead of deprecated Annotation\Route

--- budget-pwa/backend/src/Controller/StatsController.php
+++ budget-pwa/backend/src/Controller/StatsController.php
@@ -8,7 +8,7 @@ use App\Repository\TransactionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class StatsController extends AbstractController
 {
